6edf86d167412245561a07a0aea38a4d5404d363
Angel Antonio Lux Pérez <lux.antonio052916@gmail.com>
2021-10-19 18:46:07 -0600

    [ADDED] Primera Fase: Bitacora Registro y login

--- tienda/usuario/urls.py
+++ tienda/usuario/urls.py
@@ -5,4 +5,9 @@ urlpatterns = [
     path('registro/', views.registro, name="registro"),
     path('login/', views.login, name="login"),
     path('home/', views.home, name="home"),
+    path('list/', views.listaregistro, name="Lista"),
+    path('delete/<int:id>/', views.eliminar, name="Eliminar"),
+    path('desbloquear/<int:id>/', views.desbloquear, name="desbloquear"),
+    path('bitacora/', views.bitacora, name="Bitacora"),
+    
 ]
--- tienda/usuario/views.py
+++ tienda/usuario/views.py
@@ -15,6 +15,7 @@ def registro(request):
             data = form.cleaned_data
             username = data['username']
             registrar_bitacora("Usuario registrado exitosamente.", Usuario.objects.get(username=username))
+            messages.add_message(request, level=messages.SUCCESS, message="Usuario Registrado con Exito")
             return redirect('/login')
     else:
         form = RegistroForm()
@@ -35,23 +36,61 @@ def login(request):
             except:
                 auth = None
             if auth:
-                if(auth.contrasena == contrasena):
-                    registrar_bitacora("El usuario " + str(username) + " ha ingresado al sistema exitosamente.", auth)
-                    sweetify.success(request, 'Bienvenido al sistema.')
-                    return redirect('/home')
+                if(auth.intentos<6):
+                    if(auth.contrasena == contrasena):
+                        registrar_bitacora("El usuario " + str(username) + " ha ingresado al sistema exitosamente.", auth)
+                        sweetify.success(request, 'Bienvenido al sistema.')
+                        auth.intentos = 0
+                        auth.save()
+                        return redirect('/home')
+                    else:
+                        auth.intentos = auth.intentos + 1
+                        auth.save()
+                        registrar_bitacora("El usuario " + str(username) + " ha intentado ingresar al sistema.", auth)
+                        
+                        messages.add_message(request, level=messages.SUCCESS, message="Usuario o Contraseña incorrecta")
+                        if(auth.intentos == 3):
+                            #Motrar mensaje de advertencia
+                            messages.add_message(request, level=messages.SUCCESS, message="ADVERTENCIA: actualmente lleva 3 intentos para ingresar, a los 6 se bloqueara su usuario")
+                            return redirect('/login')
+                           
+                        if(auth.intentos == 6):
+                            messages.add_message(request, level=messages.SUCCESS, message="Ha llegado al limite de intentos. Se ha bloqueado su usuario")
+                            registrar_bitacora("El usuario " + str(username) + " esta bloqueado actualmente.", auth)
+                            return redirect('/login')
+                            
+                        # return redirect('/')
                 else:
-                    auth.intentos = auth.intentos + 1
-                    auth.save()
-                    registrar_bitacora("El usuario " + str(username) + " ha intentado ingresar al sistema.", auth)
-                    sweetify.info(request, 'Usuario o contraseña incorrecto', button='Ok', timer=3000)
-                    # return redirect('/')
+                    messages.add_message(request, level=messages.SUCCESS, message="USUARIO BLOQUEADO: Consulte con un usuario administrador para poder desbloquear su usuario")
                     return redirect('/login')
+                    
             else:
+    
                 return redirect('/login')
     else:
         form = LoginForm()
 
     return render(request, 'auth/login.html', {'forms_login':form})
+
+def listaregistro(request):
+    context = {'lista_registros' : Usuario.objects.all()}
+    return render(request, 'auth/listaregistro.html', context)
+
+def eliminar(request, id):
+    Usuario2 = Usuario.objects.get(pk=id)
+    Usuario2.delete()
+    return redirect('/list')
+
+def desbloquear(request, id):
+    Usuario2 = Usuario.objects.get(pk=id)
+    Usuario2.intentos = 0
+    Usuario2.save()
+    #registrar_bitacora("Se ha desbloqueado el usuario " + str(username), Usuario.objects.get(username=username))
+    return redirect('/list')
+
+def bitacora(request):
+    context2 = {'bitacora' : Bitacora.objects.all()}
+    return render(request, 'auth/bitacora.html', context2)
 
 def home(request):
     return render(request, 'auth/index.html')
